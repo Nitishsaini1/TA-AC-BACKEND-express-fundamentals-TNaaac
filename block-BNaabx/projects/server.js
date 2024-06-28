0db17672ffc7cdcd2e5554b6989f6d843e17392b
Nitish Kumar <ns7410995@gmail.com>
2024-06-29 03:14:24 +0530

    Assignment III

---
+++ block-BNaabx/projects/server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var logger = require("morgan");
+var cookieparser = require("cookie-parser");
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.static());
+app.use(cookieparser())
+
+app.get('/',(req,res)=>{
+    res.json(req.body)
+})
+
+app.listen(4000,()=>{
+    console.log('listening to 4k')
+})
